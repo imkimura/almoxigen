a48b8199ce05a1e5b121556575663f7163c77df8
elton-tanaka <elton.tanaka@univem.edu.br>
2020-04-25 21:27:28 -0300

    added healthunit table

---
+++ database/migrations/2020_04_26_001347_create_health_unit_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHealthUnitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('health_unit', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('health_unit');
+    }
+}
