bf5ea215d0762d57fb0a19ce12f2628e9860b760
elton-tanaka <elton.tanaka@univem.edu.br>
2020-05-18 00:37:23 -0300

    added reports page

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -18,6 +18,35 @@ class ReportController extends Controller
 
     public function index()
     {
-        return view('admin.report.index');
+        $patients = $this->patientModel->all();
+
+        $patients = $patients->map(function($order) {
+            $material = $this->materialModel->find($order->material_id)->first();
+            $healthUnit = $this->healthUnitModel->find($order->health_unit_id)->first();
+
+            $order->nmMaterial = $material->name;
+            $order->nmHealthUnit = $healthUnit->name;
+
+            return $order;
+        });
+
+        $healthUnits = $this->healthUnitModel->all();
+
+        $healthUnits = $healthUnits->map(function($order) {
+            $patientsCount = array();
+            $materialsCount[] = array();
+            $patients = $this->patientsModel;
+            foreach ($patients as $patient) {
+                $patientsCount[$order->health_unit_id] = ($patientsCount[$order->health_unit_id]) ? +1 : 0;
+                $materialsCount[$order->health_unit_id][$order->material_id] = ($materialsCount[$order->health_unit_id][$order->material_id]) ? +1 : 0;
+            }
+            
+            $order->patientsCount = $patientsCount;
+            $order->materialsCount = $materialsCount;
+            return $order;
+        });
+
+        return view('admin.report.index', ['patients' => $patients,
+                                            'healthUnits' => $healthUnits]) ;
     }
 }
--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -2,6 +2,84 @@
 
 @section('content')
 <div class="row" style="padding: 60px 47px;">
-
+    <div class="card" style="width: 100%;">
+    <h3>Pacientes</h3>
+        <!-- /.card-header -->
+        <div class="card-body" >
+            <table id="table-patients" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Cpf</th>
+                    <th>Material</th>
+                    <th>Unidade de Saúde</th>
+                    <th style="width: 40px"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($patients as $patient)
+                <tr>
+                    <td>{{ $patient->name }}</td>
+                    <td>{{ $patient->cpf }}</td>
+                    <td>{{ $patient->nmMaterial }}</td>
+                    <td>{{ $patient->nmHealthUnit }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+            <!-- <tfoot>
+                <tr>
+                    <th>Nome</th>
+                    <th>Cpf</th>
+                    <th>Material</th>
+                    <th>Unidade de Saúde</th>
+                    <th></th>
+                </tr>
+            </tfoot> -->
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <div class="card" style="width: 100%;">
+    <h3>Unidades</h3>
+        <!-- /.card-header -->
+        <div class="card-body" >
+            <table id="table-healthunit" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Pacientes</th>
+                    <th>Geri-P</th>
+                    <th>Geri-M</th>
+                    <th>Geri-G</th>
+                    <th>Geri-EG</th>
+                    <th>Inf-P</th>
+                    <th>Inf-M</th>
+                    <th>Inf-G</th>
+                    <th>Inf-EG</th>
+                    <th style="width: 40px"></th>
+                </tr>
+            </thead>
+            <tbody>
+            {{ $healthUnits->materialsCount }}
+            </tbody>
+            <!-- <tfoot>
+                <tr>
+                    <th>Nome</th>
+                    <th>Pacientes</th>
+                    <th>Geri-P</th>
+                    <th>Geri-M</th>
+                    <th>Geri-G</th>
+                    <th>Geri-EG</th>
+                    <th>Inf-P</th>
+                    <th>Inf-M</th>
+                    <th>Inf-G</th>
+                    <th>Inf-EG</th>
+                    <th></th>
+                </tr>
+            </tfoot> -->
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
 </div>
 @endsection
